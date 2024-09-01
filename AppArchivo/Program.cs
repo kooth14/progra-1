4169fdefb2023f2187e3daf8bd780476366bcac0
koty14 <142451100+kooth14@users.noreply.github.com>
2024-09-01 11:32:09 -0600

    Add files via upload

---
+++ AppArchivo/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace AppArchivo;
+
+public class program
+{
+    static void Main(string[] args)
+    {
+        var directorio = Directory.GetCurrentDirectory();
+        Console.WriteLine(directorio);
+        string ruta = directorio + Path.DirectorySeparatorChar + "Archivo.txt";
+
+        var file = new FileStream(ruta, FileMode.OpenOrCreate);
+        //for(int i =65; i<= 90; i++)
+        //{
+        //    file.WriteByte((byte)i + );    
+        //}
+        int j;
+        while((j = file.ReadByte()) != -1)
+        {
+            Console.WriteLine((char)j);
+        }
+        file.Close();
+    }
+}
---
+++ FT/Planilla de pago/Form1.cs
@@ -0,0 +1,41 @@
+namespace Planilla_de_pago
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            //del nombre
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //categoria
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //guardar
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //editar
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //buscar
+        }
+
+        private void dtPago_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //planilla
+        }
+    }
+}
